151b7db0f716683916ae39a8cc5aa00781d09aa6
Vishal Soraganvi <99650261+VishalSoraganvi@users.noreply.github.com>
2022-10-15 13:38:25 +0530

    This is the commit in which there are all the files including switch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,91 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import PropTypes from 'prop-types'
-
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import PropTypes from "prop-types";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import Accordian from "./components/Accordian";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#0e243a";
+      showAlert("Dark mode activated", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode activated", "success");
+    }
+  };
+  const toggleModeBlack = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#040404";
+      TextForm.getElementsByClassName("btn-primary").style.backgroundColor =
+        "grey";
+      showAlert("Dark black mode activated", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.getElementsByClassName("btn-primary").style.backgroundColor =
+        "#0d6efd";
+      showAlert("Light mode activated", "success");
+    }
+  };
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
   return (
     <>
-     <Navbar />
+      <Router>
+        <Navbar
+          title="textUtils"
+          mode={mode}
+          toggleMode={toggleMode}
+          toggleModeBlack={toggleModeBlack}
+        />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/about" element={<Accordian />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <TextForm
+                mode={mode}
+                toggleModeBlack={toggleModeBlack}
+                showAlert={showAlert}
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
 
 Navbar.propTypes = {
-  title :PropTypes.string,
-  home : PropTypes.string,
-  about : PropTypes.string
-}
+  title: PropTypes.string,
+  home: PropTypes.string,
+  about: PropTypes.string,
+};
 
 Navbar.defaultProps = {
   title: "Enter Title",
-  home : "Home",
-  about: "About"  
-}
+  home: "Home",
+  about: "About",
+};
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("converted to Uppercase", "success");
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("converted to lowerrcase", "success");
+  };
+  const handleReverse = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+    props.showAlert("reversed the text", "success");
+  };
+
+  const changes = (event) => {
+    setText(event.target.value);
+  };
+  const handleClear = () => {
+    setText("");
+    props.showAlert("Text Cleared", "success");
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            onChange={changes}
+            value={text}
+            id="myBox"
+            rows="8"
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "grey",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+          ></textarea>
+        </div>
+        <button className={`btn btn-primary mx-2`} onClick={handleUpClick}>
+          convert to Uppercase
+        </button>
+        <button className="btn btn-primary" onClick={handleLoClick}>
+          convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleReverse}>
+          reverse
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClear}>
+          Clear
+        </button>
+      </div>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "light" ? "black" : "white",
+        }}
+      >
+        <h2>Your Text Summary</h2>
+        <p>
+          {text === "" ? "0 " : text.split(" ").length} words and {text.length}{" "}
+          characters
+        </p>
+        <p>{0.08 * text.split(" ").length}mins to read</p>
+      </div>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "light" ? "black" : "white",
+        }}
+      >
+        <h2>Preview</h2>
+        <p>{text.length === 0 ? "Please enter something" : text}</p>
+      </div>
+    </>
+  );
+}
